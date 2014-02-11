4a35f04d160dd28e09e2f3b92850a818d9e27e3e
Marius Butuc <marius.butuc@gmail.com>
2014-02-11 03:07:16 -0500

    Refactor #1: Views shouldn't know anything about data
    ...transfer the hardcoded data one level down, into the relevant controllers.

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,11 +1,86 @@
 module.exports.homelist = function(req, res) {
-  res.render('locations-list', { title: 'Home' });
+  res.render('locations-list', {
+    title: 'Loc8r - find a place to work with wifi',
+    pageHeader: {
+      title: 'Loc8r',
+      strapline: 'Find places to work with wifi near you!'
+    },
+    sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
+    locations: [{
+      name: 'Starcups',
+      address: '125 High Street, Reading, RG6 1PS',
+      rating: 3,
+      facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+      distance: '100m'
+    },{
+      name: 'Cafe Hero',
+      address: '125 High Street, Reading, RG6 1PS',
+      rating: 4,
+      facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+      distance: '200m'
+    },{
+      name: 'Burger Queen',
+      address: '125 High Street, Reading, RG6 1PS',
+      rating: 2,
+      facilities: ['Food', 'Premium wifi'],
+      distance: '250m'
+    }]
+  });
 };
 
 module.exports.locationInfo = function(req, res) {
-  res.render('location-info', { title: 'Location Info' });
+  res.render('location-info', {
+    title: 'Starcups',
+    sidebar: {
+      context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
+      cta: 'If you\'ve been here and you like it - or if you don\'t - please leave a review to help other people just like you.'
+    },
+    location: {
+      name: 'Starcups',
+      address: '125 High Street, Reading, RG6 1PS',
+      rating: 3,
+      facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+      coords: {
+        lat: 43.645051,
+        lng: -79.398175
+      },
+      openingTimes: [{
+        days: 'Monday - Friday',
+        opening: '7:00am',
+        closing: '7:00pm',
+        closed: false
+      },{
+        days: 'Saturday',
+        opening: '8:00am',
+        closing: '5:00pm',
+        closed: false
+      },{
+        days: 'Sunday',
+        closed: true
+      }],
+      reviews: [{
+        rating: 3,
+        author: 'Charlie Chaplin',
+        timestamp: '11 February 2014',
+        comment: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
+      },{
+        rating: 5,
+        author: 'Simon Holmes',
+        timestamp: '16 July 2013',
+        comment: 'What a cool place. I can\'t say enough great things about it.'
+      }],
+
+    }
+  });
 };
 
 module.exports.addReview = function(req, res) {
-  res.render('location-review-form', { title: 'Add Review' });
+  res.render('location-review-form', {
+    title: 'Review Starcups on Loc8r',
+    pageHeader: { title: 'Review Starcups' },
+    user: {
+      displayName: 'Marius Butuc'
+    }
+
+  });
 };
