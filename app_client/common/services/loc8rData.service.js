a63f00d2bb155a318ed1e5435243f9bef88e0034
Marius Butuc <marius.butuc@gmail.com>
2014-12-16 08:50:50 -0500

    Improving browser performance
      * use IIFEs to avoid polluting the global scope
      * manual dependency injection to survive minification
      * minify and concatenate scripts with UglifyJS

--- app_client/common/services/loc8rData.service.js
+++ app_client/common/services/loc8rData.service.js
@@ -4,6 +4,7 @@
     .module('loc8rApp')
     .service('loc8rData', loc8rData);
 
+  loc8rData.$inject = ['$http'];
   function loc8rData ($http) {
     var locationByCoords = function(lat, lng) {
       query_api = '/api/locations?lng=' + lng + '&lat=' + lat + '&maxDistance=50000'
--- app_client/home/home.controller.js
+++ app_client/home/home.controller.js
@@ -4,6 +4,8 @@
     .module('loc8rApp')
     .controller('homeCtrl', homeCtrl);
 
+  // Manually inject dependencies to protect against minification
+  homeCtrl.$inject = ['$scope', 'loc8rData', 'geolocation'];
   function homeCtrl ($scope, loc8rData, geolocation) {
     // use ViewModel
     var vm = this;
